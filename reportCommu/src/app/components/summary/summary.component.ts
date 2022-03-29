171511aa0f53d24acc2313a9429cc795fef06ecc
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-03-29 01:52:52 +0100

    commit

--- reportCommu/src/app/components/summary/summary.component.ts
+++ reportCommu/src/app/components/summary/summary.component.ts
@@ -110,36 +110,36 @@ export class SummaryComponent implements OnInit {
   
   /*** Part to send email */
   
-  sendEmail(){
-    //The notes of the user are part in the body
-    this.body = this.notes;
+  // sendEmail(){
+  //   //The notes of the user are part in the body
+  //   this.body = this.notes;
 
-       //check if the mobile has an app to send email //**The function sent email works but in the phone no in the emulator */
-       this.composer.isAvailable().then(function() {
-        console.log("email available");
-          }, function() {
-        console.log("email not availabl");
-      });
+  //      //check if the mobile has an app to send email //**The function sent email works but in the phone no in the emulator */
+  //      this.composer.isAvailable().then(function() {
+  //       console.log("email available");
+  //         }, function() {
+  //       console.log("email not availabl");
+  //     });
 
-     let email = {
-      to: '21520@student.dorset-college.ie  ',  //this.to //**but not implement coz it is a student project */
-      // cc: 'nafnu@hotmail.com', 
-      // bcc: ['john@doe.com', 'jane@doe.com'],
-      // attachments: [
-      //   'file://img/logo.png',
-      //   'res://icon.png',
-      //   'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
-      //   'file://README.pdf'
-      // ],
-      subject: 'Test Email Ionic',  //this.incident
-      body: 'How are you? I am sending this message in order to resolve a problem.', //this.body
-      isHtml: true, 
-      app:"Gmail"
-    };
-    this.composer.open(email);
+  //    let email = {
+  //     to: '21520@student.dorset-college.ie  ',  //this.to //**but not implement coz it is a student project */
+  //     // cc: 'nafnu@hotmail.com', 
+  //     // bcc: ['john@doe.com', 'jane@doe.com'],
+  //     // attachments: [
+  //     //   'file://img/logo.png',
+  //     //   'res://icon.png',
+  //     //   'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+  //     //   'file://README.pdf'
+  //     // ],
+  //     subject: 'Test Email Ionic',  //this.incident
+  //     body: 'How are you? I am sending this message in order to resolve a problem.', //this.body
+  //     isHtml: true, 
+  //     app:"Gmail"
+  //   };
+  //   this.composer.open(email);
 
     
-  }
+  // }
 
 
 }
